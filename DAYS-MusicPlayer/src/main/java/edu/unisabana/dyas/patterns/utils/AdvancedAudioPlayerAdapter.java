a8b92a701d848cb6a32f964a3e6eb89c1dd6d068
juanMS <285juanda@gmail.com>
2024-08-17 12:20:54 -0500

    Refactorizar código para utilizar el patrón Adapter en el reproductor de audio

--- DAYS-MusicPlayer/src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapter.java
+++ DAYS-MusicPlayer/src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapter.java
@@ -3,13 +3,13 @@ package edu.unisabana.dyas.patterns.utils;
 public class AdvancedAudioPlayerAdapter implements AudioPlayer {
     private AdvancedAudioPlayer advancedAudioPlayer;
 
-    public AdvancedAudioPlayerAdapter (AdvancedAudioPlayer advancedAudioPlayer) {
+    public AdvancedAudioPlayerAdapter(AdvancedAudioPlayer advancedAudioPlayer) {
         this.advancedAudioPlayer = advancedAudioPlayer;
     }
 
     @Override
     public void play(String audioType, String fileName) {
-        if(audioType.toLowerCase().equals("mp4")) {
+        if (audioType.toLowerCase().equals("mp4")) {
             advancedAudioPlayer.playMp4(fileName);
         } else if (audioType.toLowerCase().equals("vlc")) {
             advancedAudioPlayer.playVlc(fileName);
